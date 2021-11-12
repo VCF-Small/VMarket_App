a7d58b44d342a224a0ee47bbd44c594ed569c494
ankit <ac2000choudhary@gmail.com>
2021-11-12 23:11:32 +0530

    Home Screen Modefied

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -5,14 +5,20 @@ import { StatusBar, StyleSheet, Text, View } from 'react-native';
 const HomeScreen:React.FC<{navigation: any}> = ({navigation}) => {
 
     return (
-        <View>
+        <View style={styles.Container}>
             <StatusBar backgroundColor='#f4338f' barStyle="light-content"/>
             <Text>Home Screen</Text>
         </View>
     )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    Container: {
+        flex: 1, 
+        justifyContent: 'center', 
+        alignItems: 'center'
+    }
+});
 
 export default HomeScreen;
 
