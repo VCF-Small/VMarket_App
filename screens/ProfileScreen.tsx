dc281f9205e7bc67146caa5d2403a90f79706301
ankit <ac2000choudhary@gmail.com>
2021-11-25 01:06:10 +0530

    getting data

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import jwt_decode from "jwt-decode";
+
 
 const ProfileScreen = () => {
+    const [userData, setUserData]: any =useState();
+
+
+
+    useEffect(() => {
+        const getData = async () =>{
+            const data: any = await AsyncStorage.getItem('@LoggedIn');
+            const user: any = jwt_decode(data);
+            setUserData(user.id);
+        }
+        getData();
+    },[])
+    
     return (
         <View style={styles.Container}>
             <Text>Profile Screen</Text>
+            <Text>{userData?.firstname +" "+ userData?.lastname}</Text>
         </View>
     )
 }
